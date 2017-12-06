7f68526164189ca9f62b63294e85b22107f52936
Jay <jay.patel@idfy.com>
2017-12-06 23:28:10 +0530

    Using zxing

--- app/src/main/java/com/jay/barcodereader/MainActivity.java
+++ app/src/main/java/com/jay/barcodereader/MainActivity.java
@@ -2,15 +2,28 @@ package com.jay.barcodereader;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.tapadoo.alerter.Alerter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +33,9 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn_scan).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, ScanActivity.class));
+                if (checkAndRequestPermissions()) {
+                    new IntentIntegrator(MainActivity.this).initiateScan();
+                }
             }
         });
     }
@@ -47,5 +62,59 @@ public class MainActivity extends AppCompatActivity {
             winParams.flags &= ~bits;
         }
         win.setAttributes(winParams);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //retrieve scan result
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (scanningResult != null) {
+            //we have a result
+            String scanContent = scanningResult.getContents();
+            // process received data
+            if (scanContent != null && !scanContent.isEmpty()) {
+                ((TextView) findViewById(R.id.text_view_result)).setText(scanningResult.getContents());
+            } else {
+                Alerter.create(MainActivity.this)
+                        .setTitle("Barcode Reader")
+                        .setText("Scan Cancelled")
+                        .setBackgroundColorRes(R.color.colorPrimaryDark)
+                        .show();
+            }
+        } else {
+            Alerter.create(MainActivity.this)
+                    .setTitle("Barcode Reader")
+                    .setText("No scan data received!")
+                    .setBackgroundColorRes(R.color.colorPrimaryDark)
+                    .show();
+        }
+    }
+
+    private boolean checkAndRequestPermissions() {
+        int camera = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+
+        if (camera != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(android.Manifest.permission.CAMERA);
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_ID_MULTIPLE_PERMISSIONS: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    new IntentIntegrator(MainActivity.this).initiateScan();
+                }
+                return;
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/jay/barcodereader/MyApplication.java
@@ -0,0 +1,14 @@
+package com.jay.barcodereader;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by jaypatel on 06/12/17.
+ */
+
+public class MyApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
