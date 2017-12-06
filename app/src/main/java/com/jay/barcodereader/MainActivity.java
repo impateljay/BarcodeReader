e2015754720847ee3109f26cd9d4922f8a2672c8
Jay <jay.patel@idfy.com>
2017-12-07 00:54:02 +0530

    published to play store v1.0 beta

--- app/src/main/java/com/jay/barcodereader/MainActivity.java
+++ app/src/main/java/com/jay/barcodereader/MainActivity.java
@@ -9,11 +9,21 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+import com.google.firebase.crash.FirebaseCrash;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.tapadoo.alerter.Alerter;
@@ -21,9 +31,11 @@ import com.tapadoo.alerter.Alerter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RewardedVideoAdListener {
 
     private static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
+    private AdView mAdView;
+    private RewardedVideoAd mAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +51,124 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                mAdView.setVisibility(View.VISIBLE);
+                FirebaseCrash.log("onAdLoaded()");
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+                FirebaseCrash.log("onAdFailedToLoad() : " + errorCode);
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+                FirebaseCrash.log("onAdOpened()");
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+                FirebaseCrash.log("onAdLeftApplication()");
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the user is about to return
+                // to the app after tapping on an ad.
+                FirebaseCrash.log("onAdClosed()");
+            }
+        });
+        mAd = MobileAds.getRewardedVideoAdInstance(this);
+        mAd.setRewardedVideoAdListener(this);
+        loadRewardedVideoAd();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (mAd.isLoaded()) {
+            mAd.show();
+        } else {
+            loadRewardedVideoAd();
+            if (mAd.isLoaded()) {
+                mAd.show();
+            }
+            mAd.show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_exit) {
+            if (mAd.isLoaded()) {
+                mAd.show();
+            } else {
+                loadRewardedVideoAd();
+                if (mAd.isLoaded()) {
+                    mAd.show();
+                }
+                mAd.show();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        mAd.resume(this);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mAd.pause(this);
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mAd.isLoaded()) {
+            mAd.show();
+        } else {
+            loadRewardedVideoAd();
+            if (mAd.isLoaded()) {
+                mAd.show();
+            }
+            mAd.show();
+        }
+        mAd.destroy(this);
+        super.onDestroy();
+    }
+
+    private void loadRewardedVideoAd() {
+        mAd.loadAd("ca-app-pub-7577307801270101/7401484749", new AdRequest.Builder().build());
+    }
+
 
     private void transparentToolbar() {
         if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
@@ -77,14 +207,14 @@ public class MainActivity extends AppCompatActivity {
                 ((TextView) findViewById(R.id.text_view_result)).setText(scanningResult.getContents());
             } else {
                 Alerter.create(MainActivity.this)
-                        .setTitle("Barcode Reader")
+                        .setTitle("QR & Barcode Scanner")
                         .setText("Scan Cancelled")
                         .setBackgroundColorRes(R.color.colorPrimaryDark)
                         .show();
             }
         } else {
             Alerter.create(MainActivity.this)
-                    .setTitle("Barcode Reader")
+                    .setTitle("QR & Barcode Scanner")
                     .setText("No scan data received!")
                     .setBackgroundColorRes(R.color.colorPrimaryDark)
                     .show();
@@ -106,15 +236,57 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case REQUEST_ID_MULTIPLE_PERMISSIONS: {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     new IntentIntegrator(MainActivity.this).initiateScan();
                 }
-                return;
             }
         }
+    }
+
+    @Override
+    public void onRewardedVideoAdLoaded() {
+        FirebaseCrash.log("onRewardedVideoAdLoaded()");
+    }
+
+    @Override
+    public void onRewardedVideoAdOpened() {
+        FirebaseCrash.log("onRewardedVideoAdOpened()");
+    }
+
+    @Override
+    public void onRewardedVideoStarted() {
+        FirebaseCrash.log("onRewardedVideoStarted()");
+    }
+
+    @Override
+    public void onRewardedVideoAdClosed() {
+        FirebaseCrash.log("onRewardedVideoAdClosed()");
+    }
+
+    @Override
+    public void onRewarded(RewardItem rewardItem) {
+        FirebaseCrash.log("onRewarded() : " + rewardItem);
+        try {
+            Alerter.create(MainActivity.this)
+                    .setTitle("QR & Barcode Scanner")
+                    .setText("Thanks for watching ad:)\nClick on ad to help developer")
+                    .setBackgroundColorRes(R.color.colorPrimaryDark)
+                    .show();
+        } catch (Exception e) {
+            FirebaseCrash.log(e.getMessage());
+        }
+    }
+
+    @Override
+    public void onRewardedVideoAdLeftApplication() {
+        FirebaseCrash.log("onRewardedVideoAdLeftApplication()");
+    }
+
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int i) {
+        FirebaseCrash.log("onRewardedVideoAdFailedToLoad() : " + i);
     }
 }
--- app/src/main/java/com/jay/barcodereader/MyApplication.java
+++ app/src/main/java/com/jay/barcodereader/MyApplication.java
@@ -2,6 +2,11 @@ package com.jay.barcodereader;
 
 import android.support.multidex.MultiDexApplication;
 
+import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.ads.MobileAds;
+
+import io.fabric.sdk.android.Fabric;
+
 /**
  * Created by jaypatel on 06/12/17.
  */
@@ -10,5 +15,7 @@ public class MyApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
+        MobileAds.initialize(this, "ca-app-pub-7577307801270101~4063029051");
     }
 }
